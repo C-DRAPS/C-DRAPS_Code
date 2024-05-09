9b41be9fa92be23a58a7bee7dcc0b4dad3c174ba
Fermin Ramos <88788004+FerminRamos@users.noreply.github.com>
2024-05-09 10:09:21 -0600

    Create storage.py

---
+++ Miscellaneous Helper Functions/File Searcher/storage.py
@@ -0,0 +1,89 @@
+# TODO: This file stores metaData needed for the search algorithm, including:
+#       1. raw file path
+#       2. row index location: baseYear
+#       3. row index location: county
+#       4. row index location: state
+#       5. row index location: disasterType
+#  Paths reference local copies of GitHub data repository. Change these paths to match your own.
+
+
+# TODO: "FEMA Disasters -22years.csv"
+def getPath_FEMA_22Years():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Raw Datasets\\FEMA Disasters - 22years.csv", 4, 18, 2, 6]
+
+
+# TODO: 2000-2009
+def getPath_FEMA_2000_2009():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v1\\FEMA Disasters - 2000_2009.csv", 1, 3, 0, 2]
+
+
+def getPath_FEMA_2000_2009_COUNTIES():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v1\\FEMA Disasters - 2000_2009 (Counties).csv", 1, 3, 0, 2]
+
+
+def getPath_FEMA_2000_2009_RESERVATIONS():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v1\\FEMA Disasters - 2000_2009 (Reservations).csv", 1, 3, 0, 2]
+
+
+#
+# TODO: 2010-2019
+#
+def getPath_FEMA_2010_2019():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v1\\FEMA Disasters - 2010_2019.csv", 1, 3, 0, 2]
+
+
+def getPath_FEMA_2010_2019_COUNTIES():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v1\\FEMA Disasters - 2010_2019 (Counties).csv", 1, 3, 0, 2]
+
+
+def getPath_FEMA_2010_2019_RESERVATIONS():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v1\\FEMA Disasters - 2010_2019 (Reservations).csv", 1, 3, 0, 2]
+
+
+#
+# TODO: 2020+
+#
+def getPath_FEMA_2020():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v1\\FEMA Disasters - 2020_2022.csv", 1, 3, 0, 2]
+
+
+def getPath_FEMA_2020_COUNTIES():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v1\\FEMA Disasters - 2020_2022 (Counties).csv", 1, 3, 0, 2]
+
+
+def getPath_FEMA_2020_RESERVATIONS():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v1\\FEMA Disasters - 2020_2022 (Reservations).csv", 1, 3, 0, 2]
+
+
+
+
+
+
+#
+# TODO: 2000-2005
+#
+def getPath_FEMA_2000_2005_COUNTIES():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v2\\FEMA Disasters - 2000_2005.csv",1, 3, 0, 2]
+
+def getPath_FEMA_2000_2005_RESERVATIONS():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v2\\FEMA Disasters - 2000_2005.csv",1, 3, 0, 2]
+
+#
+# TODO: 2006-2014
+#
+def getPath_FEMA_2006_2014_COUNTIES():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v2\\FEMA Disasters - 2006_2014 (Counties).csv", 1, 3, 0, 2]
+
+
+def getPath_FEMA_2006_2014_RESERVATIONS():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v2\\FEMA Disasters - 2006_2014 (Reservations).csv", 1, 3, 0, 2]
+
+#
+# TODO: 2020+
+#
+def getPath_FEMA_2015_2022_COUNTIES():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v2\\FEMA Disasters - 2015_2022 (Counties).csv", 1, 3, 0, 2]
+
+
+def getPath_FEMA_2015_2022_RESERVATIONS():
+    return ["C:\\Users\\ferramos\\Desktop\\DRASTIC Databases\\DRASTIC\\Geographical Data Maps\\FEMA Disasters\\Cluster v2\\FEMA Disasters - 2015_2022 (Reservations).csv", 1, 3, 0, 2]
